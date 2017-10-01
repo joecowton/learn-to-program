2dff9f4594cae3e1bbeb45f11eefb9f6ef4b4a53
Joe Cowton <joecowton@gmail.com>
2017-10-01 14:39:20 +0100

    initial commit

--- chap07/7_1.rb
+++ chap07/7_1.rb
@@ -1,16 +1,22 @@
 puts "hello child, what do you want?"
-
+#byeCount = 0
 while true
   input = gets.chomp
-  if input == "BYE"
-    puts "LOVELY TO SEE YOU!"
-    break
-  end
+#  if input == "BYE"
+#    byeCount = byeCount + 1
+#  else
+#    byeCount = 0
+#  end
 
-  if input != input.upcase
-    puts "SPEAK UP SONNY"
-  else
-    year = 1930 + rand(21)
-    puts "NOT SINCE #{year.to_s}"
-  end
+#  if byeCount >= 3
+#    puts "LOVELY TO SEE YOU!"
+#    break
+#  end
+
+#  if input != input.upcase
+#    puts "SPEAK UP SONNY"
+#  else
+#    year = 1930 + rand(21)
+#    puts "NOT SINCE #{year.to_s}"
+#  end
 end
